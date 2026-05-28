2706de3dfceaf1f1b6cc126e0d0a5a69a884a06a
gagdario <dariog.gagliardi@gmail.com>
2026-05-28 12:57:02 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  supabaseUrl: 'https://sozsweyiloigqolyqhiq.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvenN3ZXlpbG9pZ3FvbHlxaGlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NjA3MTcsImV4cCI6MjA5NTUzNjcxN30.Ubwg4Gzb6F5Tw7fPqrg5epP11XWBrARluGpzHHPYmGY',
+  claudeModel: 'claude-sonnet-4-20250514',
+};
